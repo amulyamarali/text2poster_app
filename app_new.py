26f124f4c4b9eee1723d704732edfa6e2def24cb
Chandan Kasamsetty <34879929+chandan2300@users.noreply.github.com>
2023-07-10 19:16:54 +0530

    new_files_added

--- app_new.py
+++ app_new.py
@@ -4,11 +4,13 @@ import torch
 from diffusers import StableDiffusionPipeline
 
 st.title("Text2Poster AI")
-
-# generate imae using stable difusion 
-def gen_img_SD(input_text):
+input_text = st.text_input("Enter the product name")
+def gen_img_SD():
     pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype = torch.float16)
-    pipe = pipe(input_text).image[0]
+    pipe = pipe.to("cuda")
+    # pipe = pipe(input_text).image[0]
     image = pipe(input_text).images[0]
-    return image
-st.image(gen_img_SD("Sports Shoe"))
+    st.image(image)
+    # return image
+button = st.button("Generate",on_click=gen_img_SD)
+# st.image(gen_img_SD("Sports Shoe"))
